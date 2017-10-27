82ae2818b2ed0a8338cc7392db9d4fab86a6783f
risaco <rhia.cooper@gmail.com>
2017-10-26 21:29:22 -0500

    added Article.js

---
+++ models/Article.js
@@ -0,0 +1,29 @@
+// ***** DEPENDENCIES *****
+var mongoose = require("mongoose");
+
+// save a reference to the Schema constructor
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+
+  // This allows us to populate the Article with an associated Note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// creates model from the above Schema
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
